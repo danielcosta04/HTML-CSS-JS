4507518caa1da978d683f05bb2ec60fa4932ff59
daniel costa <dalpcosta@gmail.com>
2025-01-04 18:22:56 -0300

    alterado

---
+++ Nuvem matrix/nuvemMatrix.js
@@ -0,0 +1,29 @@
+function randonText() {
+    var text = "!@#$%^*()";
+    let letters = text[Math.floor(Math.random() * text.length)];
+    return letters;
+}
+
+function rain() {
+    let cloud = document.querySelector('.cloud');
+    let e = document.createElement('div');
+    e.classList.add('drop');
+    cloud.appendChild(e);
+
+    let left = Math.floor(Math.random() * 300);
+    let size = Math.random() * 1.5; 
+    let duration = Math.random() * 1;
+
+    e.innerText = randonText();
+    e.style.left = left + 'px';
+    e.style.fontSize = 0.5 + 'em';
+    e.style.animationDuration = 1 + duration + 's';
+
+    setTimeout(function() {
+        cloud.removeChild(e);
+    }, 2000);
+}
+
+setInterval(function() {
+    rain();
+}, 20);
